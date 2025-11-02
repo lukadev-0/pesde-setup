9beee87c725dd35d6e4b1f0fe88020e488142931
Erica Marigold <hi@devcomp.xyz>
2025-11-02 16:41:53 +0000

    chore(tsup): make the script standalone with all deps

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,5 +3,11 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	format: "esm",
-	clean: true
+	clean: true,
+	minify: true,
+
+	// make the ouput standalone (all dependencies bundled)
+	bundle: true,
+	noExternal: [/.*/],
+	shims: true
 });
