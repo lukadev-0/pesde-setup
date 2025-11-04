b14431a08bc432c4a3cbf9ebc4abc5afedaa4533
Erica Marigold <hi@devcomp.xyz>
2025-11-04 13:26:59 +0000

    feat(action): add support for caching package dirs

---
+++ src/cache.ts
@@ -0,0 +1,30 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import { join } from "node:path";
+
+export const PESDE_PACKAGE_DIRS = [
+	join(process.env.GITHUB_WORKSPACE!, "luau_packages"),
+	join(process.env.GITHUB_WORKSPACE!, "lune_packages"),
+	join(process.env.GITHUB_WORKSPACE!, "roblox_packages"),
+	join(process.env.GITHUB_WORKSPACE!, "roblox_server_packages"),
+	join(process.env.HOME!, ".pesde")
+];
+
+export async function cacheKey(): Promise<string> {
+	const hashFiles = async (...paths: string[]) => {
+		const hash = createHash("sha256");
+		for (const path of paths) {
+			const file = await readFile(path)
+				.then((contents) => contents.toString())
+				.catch((err: NodeJS.ErrnoException) =>
+					err.code == "ENOENT" ? Promise.resolve(`missing(${err.path})`) : Promise.reject(err)
+				);
+
+			hash.update(file);
+		}
+
+		return hash.digest("hex");
+	};
+
+	return `pesde-${process.platform}-${process.arch}-${hashFiles("pesde.toml", "pesde.lock")}`;
+}
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,12 @@ import { dirname } from "node:path";
 import { DownloadProvider } from "@/index.js";
 import logging from "@/logging.js";
 import { ToolManager } from "@/tool.js";
+import { cacheKey, PESDE_PACKAGE_DIRS } from "./cache.js";
 
 import { cacheDir, find } from "@actions/tool-cache";
 import * as core from "@actions/core";
+import * as cache from "@actions/cache";
+import { exit } from "node:process";
 
 export type Tool = "pesde" | "lune";
 export type Repo = { owner: string; repo: string };
@@ -21,7 +24,7 @@ async function setupTool(repo: Repo, version: string) {
 	const logger = parentLogger.child({ scope: "actions.setupTool" });
 
 	let toolPath = find(repo.repo, version);
-	if (toolPath == "") {
+	if (!toolPath) {
 		toolPath = await new ToolManager(repo.owner, repo.repo)
 			.version(version)
 			.install(DownloadProvider.Actions)
@@ -32,14 +35,24 @@ async function setupTool(repo: Repo, version: string) {
 					? cacheDir(dirname(result.path), repo.repo, result.version)
 					: (logger.error("Install failed.") as never)
 			);
-
-		// todo: optionally support packages caching
 	}
 
 	core.addPath(toolPath);
 }
 
+if (core.getState("needsCache") === "true") {
+	// post-run invocation, just cache and exit
+	await cache.saveCache(PESDE_PACKAGE_DIRS, await cacheKey());
+	core.saveState("needsCache", false);
+
+	exit(0);
+}
+
 const luneVersion = core.getInput("lune-version");
 if (luneVersion !== "") await setupTool(tools.lune, luneVersion);
 
+// todo: make caching here optional
 await setupTool(tools.pesde, core.getInput("version") || "latest");
+await cache
+	.restoreCache(PESDE_PACKAGE_DIRS, await cacheKey())
+	.then((hit) => (!hit ? core.saveState("needsCache", true) : {}));
